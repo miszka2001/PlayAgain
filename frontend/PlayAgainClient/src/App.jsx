02a09002672acc64bec1989a06981c799915f700
Michał <mmikoajek0@gmail.com>
2024-10-30 13:50:02 +0100

    Added logo component and outlet

--- frontend/PlayAgainClient/src/App.jsx
+++ frontend/PlayAgainClient/src/App.jsx
@@ -1,11 +1,15 @@
+import { Outlet } from "react-router-dom"
 import { Header } from "./components/Header"
 import { GlobalStyle } from "./components/styles/Global"
+import { Logo } from "./components/Logo"
 function App() {
 
   return (
     <>
     <GlobalStyle />
+    <Logo />
     <Header />
+    <Outlet />
     </>
   )
 }
