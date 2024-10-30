bf9ad0740c1e637e5d588fc501ac7c8ff8991d05
Michał <mmikoajek0@gmail.com>
2024-10-30 13:50:32 +0100

    Changes in tags within header

--- frontend/PlayAgainClient/src/components/Header.jsx
+++ frontend/PlayAgainClient/src/components/Header.jsx
@@ -3,10 +3,10 @@ import { StyledHeader } from "./styles/Header.styled"
 export const Header = () => {
     return (
         <StyledHeader>
-            <p>Categories</p>
-            <p>Bestsellers</p>
-            <p>Sing in</p>
-            <p>Log in</p>
+            <div>Categories</div>
+            <div>Bestsellers</div>
+            <div>Sing in</div>
+            <div>Log in</div>
         </StyledHeader>
     )
 }
