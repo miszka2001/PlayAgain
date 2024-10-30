4c7b98112a9057866e4a42969d3eaa3a72e34141
Michał <mmikoajek0@gmail.com>
2024-10-30 13:48:53 +0100

    Added styling for header

--- frontend/PlayAgainClient/src/components/styles/Header.styled.js
+++ frontend/PlayAgainClient/src/components/styles/Header.styled.js
@@ -1,15 +1,37 @@
 import styled from "styled-components";
 
-export const StyledHeader = styled.div`
+export const StyledHeader = styled.nav`
     position: fixed;
-    top: 0%;
+    top: 4%;
     left: 0%;
     right: 0%;
     width: 100%;
     display: flex;
     justify-content: space-between;
-    p {
+    border-top: 2px solid blue;
+    border-bottom: 2px solid blue;
+    transition: border-color 0.3s ease;
+    background-color: black;
+    color: lightblue;
+
+    &:hover {
+        border-color: orangered;
+    }
+
+
+    div {
         margin: 20px;
         cursor: pointer;
+        color: white;
+        font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
     }
+
+    div:hover {
+        color: orangered;
+    }
+
+    div:active {
+        color: blue;
+    }
+
 `
